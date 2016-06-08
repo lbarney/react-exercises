58ed0af8d63e04fcd1eae929320d2cc2c00d33bb
Lance Barney <lbarney@littleappletech.com>
2016-06-07 21:23:35 -0600

    update

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 var Greeter = React.createClass({
+	getInitialState: function(){
+		return {
+			greeting: ""
+		}
+	},
+	handleChange: function(event){
+		this.setState({greeting: event.target.value})
+	},
 	render: function (){
-		 return(
+		return(
 		 	<div>
 		 		<div>
-		 			<input type = "text" value = "Name"></input>
+		 			<input type = "text" value = {this.state.greeting} onChange = {this.handleChange}/>
+		 			<h1>Welcome to React, {this.state.greeting}</h1>
 		 		</div>
 		 	</div>
-		 	) 
-	}
+		 		) 
+		}	
 });
 
 
 
-var HelloWorld = React.createClass({
+var HelloWorld = React.createClass({  //HelloWorld Component
        render: function(){
        	return(
        		<div>
@@ -23,24 +32,17 @@ var HelloWorld = React.createClass({
        }
       });
 
-     var Components = React.createClass({
+     var Components = React.createClass({  // This is where I put my components in-order to render in the ReactDOM.render
        render: function(){
        	return(
        		<div>
        		<HelloWorld/>
        		<Greeter/>
        		</div>
-
-       		)
+				)
        }
       }); 
 
 
 
-      ReactDOM.render(
-
-        <Components/>,
-        
-
-        document.getElementById('root')
-      );
+      ReactDOM.render(<Components/>, document.getElementById('root')); // Your are only allowed one render per file. 
